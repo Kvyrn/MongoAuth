8b5409896c67272b453b1b2fe167132da366d855
URTOJU <guglmonstar@gmail.com>
2021-06-21 16:28:49 +0200

    Initial

---
+++ src/main/java/io/github/shroompye/fancyauth/FancyAuth.java
@@ -0,0 +1,11 @@
+package io.github.shroompye.fancyauth;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FancyAuth implements ModInitializer {
+    public static final String modid = "fancy-auth";
+
+    @Override
+    public void onInitialize() {
+    }
+}
